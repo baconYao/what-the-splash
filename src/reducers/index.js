c95edd3f5f95cd858982bfe1907f4cce214638dc
peiyao <peiyaochang@qnap.com>
2020-08-27 23:46:40 +0800

    Fix typo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,12 @@
 import { combineReducers } from 'redux';
 
 import loadingReducer from './loadingReducer';
-import imageReducer from './imageReducer';
 import errorReducer from './errorReducer';
-import imagesReducer from './imageReducer';
+import imageReducer from './imageReducer';
 
 const rootReducer = combineReducers({
     isLoading: loadingReducer,
-    images: imagesReducer,
+    images: imageReducer,
     error: errorReducer
 });
 
